aa2705c3e3e718275b0ec193588d3d912e0e35f9
Firrellum <firrellum@github.com>
2025-03-24 16:04:23 +0300

    cors

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 import express from "express";
 import { WebSocketServer } from "ws";
 import { createServer } from "http";
-// import cors from "cors";
-// "express": "^4.21.2",
+import cors from "cors";
+
 
 const app = express();
 const server = createServer(app);
 const wss = new WebSocketServer({ server });
 
 
-// app.use(cors({
-//   origin: "*", 
-//   methods: ["GET", "POST"], 
-//   credentials: true 
-// }));
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST"], 
+  credentials: true 
+}));
 
 app.use(express.static("public"));
 
